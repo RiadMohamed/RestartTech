b533c0808da129e321fac04d7dc6d5c7007917c5
Riad Mohamed <riaduk1@gmail.com>
2021-03-18 14:31:25 +0200

    Implement the Floating Button in the Tab Bar and add it's handler function

--- RestartTech/Controller/TabBarController.swift
+++ RestartTech/Controller/TabBarController.swift
@@ -15,5 +15,25 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
+        setupMiddleButton()
+    }
+    
+    func setupMiddleButton() {
+        // Create the button
+        let middleBtn = UIButton(frame: CGRect(x: ((self.view.bounds.width/2)-25), y: -20, width: 50, height: 50))
+        
+        // Button Customization
+        middleBtn.setImage(UIImage(named: "bellman_bottom-icon"), for: .normal)
+        
+        // Register the button and the tap event
+        self.tabBar.addSubview(middleBtn)
+        middleBtn.addTarget(self, action: #selector(self.menuButtonTapped), for: .touchUpInside)
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    @objc func menuButtonTapped(sender: UIButton) {
+        // What will happen when the middle buttonis tapped
+        print("Floating Button Tapped")
     }
 }
